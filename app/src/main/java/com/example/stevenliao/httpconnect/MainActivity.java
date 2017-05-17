88a36c4fe21d4449b94a0970d2231b3bdf4a66e8
StevenLiao <slamslamslam12411@gmail.com>
2017-05-17 15:17:59 +0800

    5/17 - http2

--- app/src/main/java/com/example/stevenliao/httpconnect/MainActivity.java
+++ app/src/main/java/com/example/stevenliao/httpconnect/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.stevenliao.httpconnect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOError;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    String connect = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            URL url = new URL(connect);
+
+        } catch (MalformedURLException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
     }
 }
