e9f3ce51abc5cc6a627cdb2f9b0390c5ed6567f5
StevenLiao <slamslamslam12411@gmail.com>
2017-05-17 17:32:02 +0800

    5/17 - http2

--- app/src/main/java/com/example/stevenliao/httpconnect/MainActivity.java
+++ app/src/main/java/com/example/stevenliao/httpconnect/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.stevenliao.httpconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.widget.EditText;
 
 import java.io.IOError;
@@ -13,16 +14,20 @@ public class MainActivity extends AppCompatActivity {
 
     String connect = "";
     EditText Name;
+    EditText IP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
+        Name = (EditText) findViewById(R.id.editText2);
+        IP = (EditText) findViewById(R.id.editText3);
+        String NameCon = Name.getText().toString();
+        String IPcon = IP.getText().toString();
+        connect = IPcon + "?Name=" + NameCon;
         try {
             URL url = new URL(connect);
+
 
         } catch (MalformedURLException e) {
           e.printStackTrace();
