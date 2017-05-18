480441f63315baf322fdfe1c3a72084a27e878a3
StevenLiao <slamslamslam12411@gmail.com>
2017-05-18 17:30:37 +0800

    5/17 - http2

--- app/src/main/java/com/example/stevenliao/httpconnect/MainActivity.java
+++ app/src/main/java/com/example/stevenliao/httpconnect/MainActivity.java
@@ -3,18 +3,26 @@ package com.example.stevenliao.httpconnect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-
 import java.io.IOError;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    String connect = "";
-    EditText Name;
-    EditText IP;
+    private String connect;
+    private EditText Name;
+    private EditText IP;
+    private Button conBtn;
+    private ConnectThread mConnectThread;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +30,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Name = (EditText) findViewById(R.id.editText2);
         IP = (EditText) findViewById(R.id.editText3);
-        String NameCon = Name.getText().toString();
-        String IPcon = IP.getText().toString();
-        connect = IPcon + "?Name=" + NameCon;
-        try {
-            URL url = new URL(connect);
+        conBtn = (Button) findViewById(R.id.button);
+
+        conBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String NameCon = Name.getText().toString();
+                String IPcon = IP.getText().toString();
+                connect = IPcon + "?Name=" + NameCon;
+                mConnectThread = new ConnectThread(connect);
+                mConnectThread.start();
+
+            }
+        });
 
 
-        } catch (MalformedURLException e) {
-          e.printStackTrace();
-        } catch (IOException e) {
-          e.printStackTrace();
+
+    }
+
+    //Connecting Thread
+    private class ConnectThread extends Thread {
+            URLConnection conn;
+        public ConnectThread(String connectIP) {
+            try {
+
+                  URL url = new URL(connectIP);
+                  conn = url.openConnection();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            try {
+
+                conn.setDoInput(true);
+                Reader in = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                int data = in.read();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
